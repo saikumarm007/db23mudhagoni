d19a78668475e3fb6f8f2f8d00a9f73d0094cc94
saikumarm007 <saikumarmudhagoni82@gmail.com>
2021-11-11 20:54:57 -0600

    crud

--- controllers/petrol.js
+++ controllers/petrol.js
@@ -4,9 +4,17 @@ exports.petrol_list = function (req, res) {
     res.send('NOT IMPLEMENTED: petrol list');
 };
 // for a specific petrol.
-exports.petrol_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: petrol detail: ' + req.params.id);
-};
+exports.petrol_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Petrol.findById(req.params.id) 
+        console.log(result);
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
 // Handle petrol create on POST.
 exports.petrol_create_post = async function (req, res) {
     console.log(req.body)
@@ -60,3 +68,26 @@ exports.petrol_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+// Handle Costume update form on PUT. 
+exports.petrol_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Petrol.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.petrol_type)  
+        toUpdate.petrol_type = req.body.petrol_type; 
+        if(req.body.quantity) toUpdate.quantity = req.body.quantity; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
+
+
